b5b8f85fbf3dce5baae14e8bcf224cdbd6a87326
Chauhan Mahaveer <45894858+chauhanmahavir@users.noreply.github.com>
2021-12-22 12:29:53 +0530

    Add files via upload

---
+++ DUMBBELL PREACHER CURL/DUMBBELL PREACHER CURL.py
@@ -0,0 +1,96 @@
+import cv2                                  #pip install opencv-python
+import mediapipe as mp                      #pip install mediapipe
+import numpy as np                          #pip install numpy
+
+mp_drawing = mp.solutions.drawing_utils
+mp_pose = mp.solutions.pose
+
+exercise_name = "DUMBBELL PREACHER CURL"
+
+cap = cv2.VideoCapture(0)
+counter = 0
+stage = None
+
+#----------------------------CALCULATE ANGLE BETWEEN THREE POINTS START-----------------------------------------------
+def calculate_angle(a,b,c):
+    a = np.array(a) # First
+    b = np.array(b) # Mid
+    c = np.array(c) # End
+
+    radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
+    angle = np.abs(radians*180.0/np.pi)
+
+    if angle >180.0:
+        angle = 360-angle
+
+    return angle
+#----------------------------CALCULATE ANGLE BETWEEN THREE POINTS END-----------------------------------------------
+
+
+with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+    while cap.isOpened():
+        ret, frame = cap.read()
+
+        # Recolor image to RGB
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image.flags.writeable = False
+
+        # Make detection
+        results = pose.process(image)
+
+        # Recolor back to BGR
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+        # Extract landmarks
+        try:
+            landmarks = results.pose_landmarks.landmark
+
+            # Get coordinates
+            L_shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+            L_elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
+            L_wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+
+            # Calculate angle
+            L_angle = calculate_angle(L_shoulder, L_elbow, L_wrist)
+
+            # Visualize angle
+            cv2.putText(image, str(L_angle),
+                           tuple(np.multiply(L_elbow, [640, 480]).astype(int)),
+                           cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
+                                )
+            # Curl counter logic
+            if L_angle > 150:
+                #print(stage)
+                #print(counter)
+                stage = "down"
+            if L_angle < 50 and stage =='down':
+                stage="up"
+                counter +=1
+                #print(stage)
+                #print(counter)
+
+        except:
+            pass
+
+        #Render Curl counter
+
+        # Rep data
+        cv2.putText(image, 'GYM', (0,30), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 0), 1, cv2.LINE_AA)         # LEFT
+        cv2.putText(image, exercise_name, (170,30),cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0,0,0), 1, cv2.LINE_AA)           # MIDDLE
+        cv2.putText(image, "COUNT :", (470,30),cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0,0,0), 1, cv2.LINE_AA)            # RIGHT
+        cv2.putText(image, str(counter), (600,30),cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0,0,0), 1, cv2.LINE_AA)            # RIGHT
+
+        # Render detections
+        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+                                mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2),
+                                mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2)
+                                 )
+
+        cv2.imshow('Live Feed', image)
+
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
